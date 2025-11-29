336f044d8ec50fe853b68d02e6eb0d9edea9a2f2
NoobTrysstuff <renepascalkasten@outlook.de>
2025-11-29 15:34:21 +0100

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    })
+    .catch(error => console.error("Footer konnte nicht geladen werden:", error));
+});
