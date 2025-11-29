4b4263d86ef118a3b44df5aed392a02b6e434cda
NoobTrysstuff <renepascalkasten@outlook.de>
2025-11-29 23:22:22 +0100

    Delete ueberschriften_banner.js

--- ueberschriften_banner.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("ueberschriften_banner.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("ueberschriften_banner").innerHTML = data;
-    })
-    .catch(error => console.error("Banner konnte nicht geladen werden:", error));
-});
